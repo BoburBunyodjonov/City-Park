d9d0731c2bd010b63fc1bdb8ab0dabfd28e1b881
Bobur Bunyodjonov <80701998+BoburBunyodjonov@users.noreply.github.com>
2024-10-04 00:18:06 +0500

    feat: same change call center icon

--- src/components/button/fixedButton.tsx
+++ src/components/button/fixedButton.tsx
@@ -1,43 +1,47 @@
-import PhoneIcon from '@mui/icons-material/Phone';
-import { IconButton } from '@mui/material';
-import { styled } from '@mui/system';
+import PhoneIcon from "@mui/icons-material/Phone";
+import { IconButton } from "@mui/material";
+import { styled } from "@mui/system";
+import callIcon from "../assets/phone-volume-solid 1.svg";
+
+import { CallCenterIcon } from "../../assets/svg";
 
 const StyledIconButton = styled(IconButton)(() => ({
-  position: 'fixed',
-  bottom: '50px',
-  right: '50px',
+  position: "fixed",
+  bottom: "50px",
+  padding: "15px",
+  right: "50px",
   zIndex: 1000,
-  backgroundColor: '#1EA582',
-  boxShadow: '0px 4px 12px rgba(0, 0, 0, 0.1)',
-  color: 'white',
+  backgroundColor: "#1EA582",
+  boxShadow: "10px 4px 12px white",
+  color: "white",
   border: "1px solid white",
-  animation: 'pulse 1.5s infinite',
-  '&:hover': {
-    backgroundColor: '#1EA582',
+  animation: "pulse 1.5s infinite",
+  "&:hover": {
+    backgroundColor: "#1EA582",
   },
-  '@keyframes pulse': {
-    '0%': {
-      transform: 'scale(1)',
-      boxShadow: '0px 0px 0px rgba(0, 0, 0, 0.2)',
+  "@keyframes pulse": {
+    "0%": {
+      transform: "scale(1)",
+      boxShadow: "0px 0px 0px rgba(0, 0, 0, 0.2)",
     },
-    '50%': {
-      transform: 'scale(1.05)',
-      boxShadow: '0px 0px 20px rgba(0, 0, 0, 0.2)',
+    "50%": {
+      transform: "scale(1.05)",
+      boxShadow: "0px 0px 20px rgba(0, 0, 0, 0.2)",
     },
-    '100%': {
-      transform: 'scale(1)',
-      boxShadow: '0px 0px 0px rgba(0, 0, 0, 0.2)',
+    "100%": {
+      transform: "scale(1)",
+      boxShadow: "0px 0px 0px rgba(0, 0, 0, 0.2)",
     },
   },
 }));
 
 const FixedButton = () => {
   return (
-    <StyledIconButton
-      aria-label="call"
-    >
-      <PhoneIcon />
-    </StyledIconButton>
+    <a href="tel:+998901234567">
+      <StyledIconButton aria-label="call">
+        <CallCenterIcon />
+      </StyledIconButton>
+    </a>
   );
 };
 
