c580584b9e8af88e83d0766d044fb1a7bc633ca5
Bobur Bunyodjonov <80701998+BoburBunyodjonov@users.noreply.github.com>
2024-10-05 18:57:05 +0500

    feat:
    Navbar bug fixed

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -13,7 +13,6 @@ export default function Navbar() {
   const [isOpen, setIsOpen] = useState<boolean>(false);
   const [isMobile, setIsMobile] = useState(false);
 
-
   const [activeSection, setActiveSection] = useState<string>("");
 
   useEffect(() => {
@@ -42,27 +41,39 @@ export default function Navbar() {
   };
 
   useEffect(() => {
-    handleResize(); 
+    handleResize();
     window.addEventListener("resize", handleResize);
     return () => window.removeEventListener("resize", handleResize);
   }, []);
-
 
   const toggleMenu = () => {
     setIsOpen((prev) => !prev);
   };
 
+  useEffect(() => {
+    if (isOpen) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "auto";
+    }
+
+    // Cleanup function: menyu yopilganda scrollni tiklash
+    return () => {
+      document.body.style.overflow = "auto";
+    };
+  }, [isOpen]);
+
   return (
     <>
-      <header className="text-gray-600 body-font bg-white border-b border-[#f1f1f1] pt-4 px-3 sticky top-0 z-50">
-        <div className="container mx-auto flex flex-wrap items-center justify-between">
+      <header className="text-gray-600 body-font bg-white border-b border-[#f1f1f1] px-3 sticky top-0 z-50">
+        <div className="container mx-auto flex flex-wrap items-center justify-between pt-3">
           <div className="flex justify-between items-center w-full md:w-auto mb-4 md:mb-0">
             <a className="title-font font-medium text-gray-900">
-              <img src={Logo} alt="Logo" className="h-10" />{" "}
+              <img src={Logo} alt="Logo" className="h-10" />
             </a>
             {isMobile ? (
               <IconButton onClick={toggleMenu}>
-                {isOpen === true ? <Close/> : <Menu/>}
+                {isOpen === true ? <Close /> : <Menu />}
               </IconButton>
             ) : (
               <IconButton onClick={toggleMenu} style={{ display: "none" }}>
@@ -71,34 +82,11 @@ export default function Navbar() {
             )}
           </div>
 
-          <nav
-            className={`menu-nav  ${
-              isOpen ? "max-h-screen " : "max-h-0"
-            } transition-max-height duration-500 ease-in-out overflow-hidden md:overflow-visible md:max-h-full absolute md:static w-full md:w-auto bg-white z-50`}
+          <div
+            className={`order-1 w-full xl:w-[30%] flex flex-col sm:flex-row justify-between xl:justify-end items-center space-y-3 sm:space-y-0 sm:space-x-4 mt-4 sm:mt-0 py-3 ${
+              isMobile && !isOpen ? "hidden" : "block"
+            }`}
           >
-            <ul className="flex flex-col md:flex-row md:space-x-5 h-full w-full md:w-auto text-center md:text-left items-center">
-              {MenuNavLink.map((nav, index) => (
-                <li key={index}>
-                  {nav.link === "/" ? (
-                    <NavLink to={nav.link}>{t(nav.title)}</NavLink>
-                  ) : (
-                    <Link
-                      to={nav.link}
-                      smooth={true}
-                      duration={500}
-                      className={`block py-2 lg:py-0 h-full cursor-pointer border-b border-transparent hover:border-primary hover:text-primary duration-150 ${
-                        activeSection === nav.link ? "text-primary" : ""
-                      }`}
-                    >
-                      {t(nav.title)}
-                    </Link>
-                  )}
-                </li>
-              ))}
-            </ul>
-          </nav>
-
-          <div className="w-full xl:w-[30%] md:order-2 flex flex-col sm:flex-row justify-between xl:justify-end items-center space-y-3 sm:space-y-0 sm:space-x-4 mt-4 sm:mt-0 py-3">
             <a href="tel:+90 534 267 64 77">
               <Button
                 startIcon={<Phone />}
@@ -114,6 +102,42 @@ export default function Navbar() {
               <MenuItem value={30}>Ingliz</MenuItem>
             </Select>
           </div>
+
+          <nav
+            className={`menu-nav ${
+              isOpen ? "h-[100vh]" : "max-h-0"
+            } transition-max-height duration-500 ease-in-out overflow-hidden md:overflow-visible md:max-h-full absolute md:static w-full md:w-auto bg-white z-50`}
+          >
+            <ul className="flex flex-col md:flex-row md:space-x-5 h-full w-full md:w-auto text-center md:text-left">
+              {MenuNavLink.map((nav, index) => (
+                <li key={index}>
+                  {nav.link === "/" ? (
+                    <NavLink
+                      className=""
+                      to={nav.link}
+                      onClick={() => isMobile && setIsOpen(false)}
+                    >
+                      {t(nav.title)}
+                    </NavLink>
+                  ) : (
+                    <Link
+                      to={nav.link}
+                      smooth={true}
+                      duration={500}
+                      className={`pb-5 block py-5 h-full cursor-pointer border-b-2 duration-150 ${
+                        activeSection === nav.link
+                          ? "border-primary text-primary "
+                          : "border-transparent hover:border-primary hover:text-primary"
+                      }`}
+                      onClick={() => isMobile && setIsOpen(false)}
+                    >
+                      {t(nav.title)}
+                    </Link>
+                  )}
+                </li>
+              ))}
+            </ul>
+          </nav>
         </div>
       </header>
     </>
